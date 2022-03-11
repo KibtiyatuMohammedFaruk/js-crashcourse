5e5859f0f4da03f0ce2c83acf8ab7ea0fdf7c080
KibtiyatuMohammedFaruk <101338447+KibtiyatuMohammedFaruk@users.noreply.github.com>
2022-03-11 09:03:23 -0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,29 @@
+let firstname = "kibtiya";
+let lastname = "Mohammed";
+let age = 20;
+
+console.log(firstname);
+console.log(lastname);
+console.log(age);
+
+console.log("My name is " + firstname + " " + lastname + ". I am " + age + " years old.");
+
+
+console.log(`My name is ${firstname} ${lastname}. I am ${age} years old.`);
+
+let fruits = ["Mango" , "Apple" , "Orange" , "Pineapple" , "Pawpaw"]
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[2]);
+console.log(fruits.push("Banana"));
+console.log(fruits);
+console.log(fruits.pop());
+console.log(fruits.pop());
+console.log(fruits);
+console.log(fruits.unshift("pear"));
+console.log(fruits);
+console.log(fruits.shift());
+console.log(fruits);
+console.log(fruits.includes("Apple"));
+console.log(fruits.includes("Ginger"));
+console.log(fruits.join("-"));
